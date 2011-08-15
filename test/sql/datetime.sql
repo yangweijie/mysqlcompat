6f3577c52de19b63bbf18c316224659d31a66746
Marti Raudsepp <marti@juffo.org>
2011-08-15 21:26:46 +0300

    Fix to_days()/from_days() on PostgreSQL 8.4+
    '0000-01-01' is not a valid date literal anymore. However, its canonical
    form was already '0001-01-01 BC' in PostgreSQL 8.2 and is accepted by
    all versions.

---
+++ test/sql/datetime.sql
@@ -0,0 +1,12 @@
+\set ECHO 0
+BEGIN;
+\i sql_bits/datetime.sql
+\set ECHO all
+
+SELECT to_days('0001-01-01');
+SELECT from_days(1);
+SELECT to_days('2011-08-15');
+SELECT from_days(734729);
+SELECT from_days(to_days(now()::date)) = now()::date;
+
+ROLLBACK;
