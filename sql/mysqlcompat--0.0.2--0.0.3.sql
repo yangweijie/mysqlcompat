4cc2fe1907473b004f86d58f457f9eeb585b29e1
Marc G Fournier <marc@coolbananas.co>
2016-09-07 11:29:07 -0700

    add upgrade functions

--- sql/mysqlcompat--0.0.2--0.0.3.sql
+++ sql/mysqlcompat--0.0.2--0.0.3.sql
@@ -1,2 +1,12 @@
 -- Dummy file. There were no functional changes in 0.0.2, just addition of 
 -- new signatures for YEAR()/MONTH() .
+
+CREATE OR REPLACE FUNCTION month(date)
+RETURNS integer AS $$
+  SELECT EXTRACT(MONTH FROM DATE($1))::integer
+$$ IMMUTABLE STRICT LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION year(timestamp without time zone)
+RETURNS integer AS $$
+  SELECT EXTRACT(YEAR FROM $1)::integer
+$$ IMMUTABLE STRICT LANGUAGE SQL;
